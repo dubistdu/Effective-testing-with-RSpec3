7989220275e39e44dcce5bad2280f8072ec67df2
Hyun Frantz <hyunfrantz@gmail.com>
2017-05-28 14:14:45 -0400

    Writing specs to running them

---
+++ spec/coffee_spec.rb
@@ -0,0 +1,15 @@
+Rspec describe 'A cup of coffee' do
+  let(:coffee) { Coffee.new }
+
+  it 'costs $1' do
+    expect(coffee.price).to_eq(1.00)
+  end
+
+  context 'with milk' do
+    before { coffee.add :milk}
+
+    it 'costs $1.25' do
+      expect(coffee.price).to_eq(1.25)
+    end
+  end
+end
