5651de460551b2368c4296a0cd801f31b9ddccfa
Hyun Frantz <hyunfrantz@gmail.com>
2017-05-29 21:42:10 -0400

    pending method

--- spec/coffee_spec.rb
+++ spec/coffee_spec.rb
@@ -31,11 +31,21 @@ RSpec.describe 'A cup of coffee' do
   end
 
 # add 'f' in front of context to make rspec focus on this particular example
-  fcontext 'with milk' do
+# fcontext'with milk' do     same as     config.filter_run_when_matching(focus:true)
+# use pending method when you are sure of future examples
+  context 'with milk' do
     before { coffee.add :milk}
 
     it 'costs $1.25' do
       expect(coffee.price).to eq(1.25)
+    end
+    it 'is light in color' do
+      pending 'Color not implemented yet'
+      expect(coffee.color).to be(:light)
+    end
+    it 'is cooler than 200F' do
+      pending 'Tempertature not implemented yet'
+      expect(coffee.temperature).to be<200.0
     end
   end
 end
